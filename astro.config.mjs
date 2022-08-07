a4f683201119e72be15bc9abd8116fc7fbc7b80e
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-08-07 14:42:56 +0200

    fix netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify/functions";
+
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
@@ -8,6 +10,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   site: "https://catherinelapsy.com",
   integrations: [vue(), tailwind(), image(), partytown(), sitemap()],
 });
