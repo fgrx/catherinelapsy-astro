85c52768035c33c15a1a594e588dbac9fd25d90f
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-08-07 17:10:16 +0200

    update astro to RC7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-import netlify from "@astrojs/netlify/functions";
-
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
-
+import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  //output: "server",
   adapter: netlify(),
   site: "https://catherinelapsy.com",
-  integrations: [vue(), tailwind(), image(), partytown(), sitemap()],
+  integrations: [vue(), tailwind(), partytown(), sitemap()],
 });
