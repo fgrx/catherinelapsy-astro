d0708966c2c3a5a119a48d496af37cb301d3c928
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-08-12 22:44:14 +0200

    improve space for navigation

--- src/vue/components/SubNav.vue
+++ src/vue/components/SubNav.vue
@@ -1,11 +1,11 @@
 <template>
   <nav
     v-show="isVisible"
-    :class="!isRaw ? 'absolute bg-dark p-2 z-20 rounded-md' : ''"
+    :class="!isRaw ? 'absolute bg-dark px-2 z-20 rounded-md' : ''"
   >
     <ul :class="!isRaw ? 'divide-y divide-gray-800' : ''">
       <li
-        :class="!isRaw ? 'p-4' : 'p-2'"
+        :class="!isRaw ? 'px-4' : 'px-2'"
         class="text-sm"
         v-for="link in links"
         :key="link.title"
@@ -14,13 +14,13 @@
           v-if="link.target === '_blank'"
           :href="link.to"
           target="link.target"
-          class="hover:text-secondary py-5"
+          class="hover:text-secondary py-3 block"
         >
           <span v-if="link.isLive">Atelier en live</span>
           {{ link.title }}</a
         >
         <a
-          class="hover:text-secondary py-5"
+          class="hover:text-secondary py-3 block"
           v-if="link.target !== '_blank'"
           :href="link.to"
         >
