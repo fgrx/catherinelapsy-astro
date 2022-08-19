6078be200574ddfd4ae577439dd80586522ab36c
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-08-19 09:56:53 +0200

    improve post display

--- src/vue/components/YoutubePlayer.vue
+++ src/vue/components/YoutubePlayer.vue
@@ -12,21 +12,6 @@ const url = computed(() => `https://www.youtube.com/embed/${video.url}`);
       {{ video.title }}
     </h3>
     <p v-if="video.description">{{ video.description }}</p>
-    <iframe
-      width="560"
-      height="315"
-      :src="url"
-      title="“YouTube"
-      video
-      player”
-      frameborder="“0”"
-      allow="“accelerometer;"
-      autoplay;
-      clipboard-write;
-      encrypted-media;
-      gyroscope;
-      picture-in-picture”
-      allowfullscreen
-    ></iframe>
+    <lite-youtube videoid="{video.url}" playlabel="{video.title}" />
   </div>
 </template>
