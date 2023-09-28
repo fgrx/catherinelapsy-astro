2c6ca4b85591fdf3b2c9af6ec53743946e04e907
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-09-28 12:29:02 +0200

    Upgrade to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,10 @@ import partytown from "@astrojs/partytown";
 import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  //output: "server",
+  output: "hybrid",
   adapter: netlify(),
   site: "https://catherinelapsy.com",
-  integrations: [
-    vue(),
-    tailwind(),
-    partytown(),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [vue(), tailwind(), partytown(), sitemap()],
 });
