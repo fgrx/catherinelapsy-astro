e96d0192321a1aad031efa04025e4cc99d7cebb3
Fabien Grignoux <fabien.grignoux@outlook.com>
2026-04-18 19:47:20 +0200

    Update tailwind version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: netlify(),
+
+  //adapter: netlify(),
   site: "https://catherinelapsy.com",
+
   image: {
     domains: ["res.cloudinary.com", "cloudinary.com"],
   },
-  integrations: [vue(), tailwind(), partytown(), sitemap()],
+
+  integrations: [vue(), partytown(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
